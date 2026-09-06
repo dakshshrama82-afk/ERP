41e4c00904dfa2cd972a697922eadfe7999e9554
Daksh Sharma <dakshshrama82@gmail.com>
2026-09-06 16:25:17 +0530

    fixing bugs

--- student_attendence.py
+++ student_attendence.py
@@ -1,19 +1,31 @@
 import json
 import student
 
-with open("student_attendence.json","r") as f:
-    data_stu = json.load(f)
-a = 0
-while True:
-    print(student.student_names[a])
-    name_student = student.student_names[a]
-    prasent = input("student is (p/a): ")
-    if prasent == "p":
-            data_stu[name_student] +=1
-    else:
-        pass
-    with open("","") as f:
-         json.dump(data_stu,f)
-    a += 1
-    if a == 60:
-        break
+def attendence_marking():
+    subject = input("enter the subject: ")
+    with open("student_attendence.json","r") as f:
+        data_stu = json.load(f)
+    a = 0
+    while True:
+        print(student.student_names[a])
+        name_student = student.student_names[a]
+        prasent = input("student is (p/a): ")
+        if prasent == "p":
+                data_stu[name_student][subject] +=1
+        else:
+            pass
+        a += 1
+        if a == 60:
+            break
+    with open("student_attendence.json","w") as f:
+        json.dump(data_stu,f,indent=4)
+attendence_marking()        
+
+
+
+
+
+
+
+
+
